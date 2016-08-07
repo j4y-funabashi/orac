a8855075e7e9cac0e54329227cbeae7f665b2063
Jay Robinson <me@j4y.co>
2016-08-07 12:56:10 +0100

    init

--- run.py
+++ run.py
@@ -1,3 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+from dot3k.menu import MenuOption
+
+class HelloWorld(MenuOption):
+
+    def redraw(self, menu):
+        menu.write_option(
+                row=1,
+                text='Hello World! How are you today?',
+                scroll=True
+                )
+
+        menu.clear_row(2)
+
+
+from dot3k.menu import Menu
+import dot3k.backlight
+import dot3k.lcd
+import time
+
+dot3k.backlight.rgb(255, 255, 255)
+
+menu = Menu(
+        structure={
+            'Hello World': HelloWorld()
+            },
+        lcd=dot3k.lcd
+        )
+
+menu.right()
+
+while 1:
+    menu.redraw()
+    time.sleep(0.01)
