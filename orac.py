cd6764d6a31c33d6703ff93da99a9e53df7c8acc
Jay Robinson <me@j4y.co>
2016-08-25 14:12:05 +0100

    start orac library with weather

---
+++ orac.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def get_weather(api_url, city, api_key):
+    url = "%s/weather?q=%s&units=metric&appid=%s" % (api_url, city, api_key)
+    print url
+
+api_url = "http://api.openweathermap.org/data/2.5"
+city = "Leeds,uk"
+api_key = ""
+get_weather(api_url, city, api_key)
