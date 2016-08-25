b84761b33f43f1b279a79fbddcfcea51518fc9be
Jay Robinson <me@j4y.co>
2016-08-25 16:35:50 +0100

    Pull current weather from openweathermap

--- orac.py
+++ orac.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from os.path import join, dirname
+from os import environ
+from dotenv import load_dotenv
+
+
+import requests
+
 def get_weather(api_url, city, api_key):
     url = "%s/weather?q=%s&units=metric&appid=%s" % (api_url, city, api_key)
-    print url
+    weather = requests.get(url).json()
+    out = "%s %sC" % (weather['weather'][0]['main'], weather['main']['temp'])
+    print(out)
 
 
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 api_url = "http://api.openweathermap.org/data/2.5"
 city = "Leeds,uk"
-api_key = ""
+api_key = environ.get("WEATHER_API_KEY")
 get_weather(api_url, city, api_key)
