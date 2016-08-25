19f3434658a45f8a103e7e6ae33bc3d1317e37f3
Jay Robinson <me@j4y.co>
2016-08-25 15:28:01 +0100

    tidy

--- orac.py
+++ orac.py
@@ -5,6 +5,9 @@ def get_weather(api_url, city, api_key):
     url = "%s/weather?q=%s&units=metric&appid=%s" % (api_url, city, api_key)
     print url
 
+
+
+
 api_url = "http://api.openweathermap.org/data/2.5"
 city = "Leeds,uk"
 api_key = ""
