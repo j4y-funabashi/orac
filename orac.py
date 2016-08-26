e1b446fa6a76b3bc43c56b9ba1714f765a0ad352
Jay Robinson <me@j4y.co>
2016-08-26 19:05:14 +0100

    dot3k

--- orac.py
+++ orac.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
-from mpd import MPDClient
+# from mpd import MPDClient
 
 def get_weather(api_url, city, api_key):
     url = "%s/weather?q=%s&units=metric&appid=%s" % (api_url, city, api_key)
@@ -14,12 +14,12 @@ def get_bus_times(stop_atco_code, app_id, app_key, api_url):
     bus_times = requests.get(url).json()
     return ["{0[aimed_departure_time]} {0[line]} {0[direction]}".format(bus) for bus in bus_times['departures']['all']]
 
-def get_mpd_playlist():
-    client = MPDClient()
-    client.timeout = 10
-    client.connect("localhost", 6600)
-    current_song = "> {0[artist]} - {0[title]}".format(client.currentsong())
-    next_song = "{0[artist]} - {0[title]}".format(client.playlistid(client.status()['nextsongid']).pop())
-    client.close()
-    client.disconnect()
-    return [current_song, next_song]
+# def get_mpd_playlist():
+    # client = MPDClient()
+    # client.timeout = 10
+    # client.connect("localhost", 6600)
+    # current_song = "> {0[artist]} - {0[title]}".format(client.currentsong())
+    # next_song = "{0[artist]} - {0[title]}".format(client.playlistid(client.status()['nextsongid']).pop())
+    # client.close()
+    # client.disconnect()
+    # return [current_song, next_song]
