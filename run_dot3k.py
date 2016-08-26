4ee5edc519e024ab1cd3f55656abd844a07bfe8d
Jay Robinson <me@j4y.co>
2016-08-26 19:06:14 +0100

    dot3k

--- run_dot3k.py
+++ run_dot3k.py
@@ -6,6 +6,11 @@ import time
 from orac import get_bus_times
 
 def redraw():
+    stop_atco_code = "450010861"
+    api_url = "http://transportapi.com/v3/uk/bus/stop"
+    app_id = environ.get("BUS_APP_ID")
+    app_key = environ.get("BUS_APP_KEY")
+
     times = get_bus_times(stop_atco_code, app_id, app_key, api_url)
     lcd.set_cursor_position(0, 0)
     lcd.write(times[0])
