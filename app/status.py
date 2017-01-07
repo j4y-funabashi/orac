05007b6edf5a9baf2ead4ff059c3bde25f06662d
Jay Robinson <me@j4y.co>
2017-01-07 23:50:33 +0000

    add ip to status

--- app/status.py
+++ app/status.py
@@ -1,6 +1,9 @@
 from dot3k.menu import MenuOption
 
 import time
+import socket
+import fcntl
+import struct
 
 class Status(MenuOption):
 
@@ -14,6 +17,18 @@ class Status(MenuOption):
                 icon="",
                 text=time.strftime(' %a %H:%M:%S ')
                 )
-        menu.clear_row(1)
+        menu.write_option(
+                row=1,
+                margin=1,
+                icon="",
+                text=self.get_ip_address()
+                )
         menu.clear_row(2)
 
+    def get_ip_address(ifname):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+        )[20:24])
