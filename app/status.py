2e0d3c2642b234cad3fac9c6eed860d9e7d80997
Jay Robinson <me@j4y.co>
2017-01-08 00:08:29 +0000

    rm ip

--- app/status.py
+++ app/status.py
@@ -1,9 +1,6 @@
 from dot3k.menu import MenuOption
 
 import time
-import socket
-import fcntl
-import struct
 
 class Status(MenuOption):
 
@@ -12,23 +9,10 @@ class Status(MenuOption):
 
     def redraw(self, menu):
         menu.write_option(
-                row=0,
+                row=1,
                 margin=1,
                 icon="",
                 text=time.strftime(' %a %H:%M:%S ')
                 )
-        menu.write_option(
-                row=1,
-                margin=1,
-                icon="",
-                text=self.get_ip_address()
-                )
+        menu.clear_row(0)
         menu.clear_row(2)
-
-    def get_ip_address(ifname):
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
-        )[20:24])
